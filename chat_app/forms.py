0d5b236c7ac78debdea619b290f2336032957d8b
naka0303 <jyunki.nakagawa@gmail.com>
2023-08-13 19:10:19 +0900

    ログイン画面デザイン変更

--- chat_app/forms.py
+++ chat_app/forms.py
@@ -22,6 +22,12 @@ class ChatroomCreationForm(forms.ModelForm):
         fields = ('chatroom_no', 'chatroom_name')
 
 class LoginForm(AuthenticationForm):
-    class Meta:
-        model = User
-        fields = ['email', 'password']
+    # class Meta:
+    #     model = User
+    #     fields = ['email', 'password']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for filed in self.fields.values():
+            filed.widget.attrs['class'] = 'form-control'
+            filed.widget.attrs['placeholder'] = filed.label
