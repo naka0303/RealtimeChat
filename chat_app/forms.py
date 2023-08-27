14ab8d6f068378ed5ad76c46d21503d000f69287
naka0303 <jyunki.nakagawa@gmail.com>
2023-08-27 14:48:51 +0900

    css修正

--- chat_app/forms.py
+++ chat_app/forms.py
@@ -15,6 +15,7 @@ class CustomUserCreationForm(UserCreationForm):
 
         for filed in self.fields.values():
             filed.widget.attrs['class'] = 'form-control'
+            filed.widget.attrs['placeholder'] = filed.label
 
 class ChatroomCreationForm(forms.ModelForm):
     class Meta:
@@ -22,9 +23,6 @@ class ChatroomCreationForm(forms.ModelForm):
         fields = ('chatroom_no', 'chatroom_name')
 
 class LoginForm(AuthenticationForm):
-    # class Meta:
-    #     model = User
-    #     fields = ['email', 'password']
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
